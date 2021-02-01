6f8a9dad3895e5b46dd3a39bf6c7477cc1759d33
Hagyeong Kim <78262460+hhagyeong@users.noreply.github.com>
2021-02-01 19:55:31 +0900

    [if문] 시험 성적

---
+++ ch02_if/no9498.java
@@ -0,0 +1,29 @@
+package ch02_if;
+
+import java.util.Scanner;
+
+/* 
+ * 주소: https://www.acmicpc.net/problem/9498
+ * 문제: 시험 성적
+ *  */
+public class no9498 {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int score = scanner.nextInt();
+
+		if (score >= 90) {
+			System.out.println("A");
+		} else if (score >= 80) {
+			System.out.println("B");
+		} else if (score >= 70) {
+			System.out.println("C");
+		} else if (score >= 60) {
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+
+	}
+
+}
